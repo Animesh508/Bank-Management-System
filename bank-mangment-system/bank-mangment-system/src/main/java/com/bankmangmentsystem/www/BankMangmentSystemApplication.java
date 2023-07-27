1d6397376722f4e09bf89160a339bc3d39fd24ea
Animesh <animeshpatil43@gmail.com>
2023-07-27 16:47:50 +0530

    banking project

---
+++ bank-mangment-system/bank-mangment-system/src/main/java/com/bankmangmentsystem/www/BankMangmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bankmangmentsystem.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMangmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMangmentSystemApplication.class, args);
+	}
+
+}
---
+++ bank-mangment-system/bank-mangment-system/src/test/java/com/bankmangmentsystem/www/BankMangmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankmangmentsystem.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMangmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
